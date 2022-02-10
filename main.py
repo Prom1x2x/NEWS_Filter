554191d737da1a01f5553425eefefef931761742
Prom1x <kirill.zhu46@gmail.com>
2022-02-10 16:02:10 +0300

    .

--- main.py
+++ main.py
@@ -7,14 +7,25 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.from_user.id, 'Hello')
-    bot.send_message(message.from_user.id, '''Что я умею?
-                                            /help - посмотреть список команд
-                                            /black - посмотреть черный список''')
+    bot.send_message(message.from_user.id, f'''Вот что я умею?
+/help - посмотреть список команд
+/black - посмотреть черный список
+/addtoblok - добавить тему/слово в черный список''')
 
 @bot.message_handler(commands=['help'])
 def help(message):
-    pass
+    bot.send_message(message.from_user.id, f'''Вот что я умею?
+/help - посмотреть список команд
+/black - посмотреть черный список
+/addtoblok - добавить тему/слово в черный список''')
 
+@bot.message_handler(commands=['black'])
+def blacklist(message):
+    bot.send_message(message.from_user.id, f'''Слова/темы в черном списке''')
+
+@bot.message_handler(commands=['addtoblok'])
+def add_to_blacklist(message):
+    bot.send_message(message.from_user.id, 'Введите слово, которое хотите добавить в черный список')
 
 if __name__ == '__main__':
     bot.polling(none_stop = True, interval = 0)
