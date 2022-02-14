6d92a2016fa2b3f4c15bac40e4e97241a1f5a94d
Proot <proot@Proot-desktop.Proot>
2022-02-14 16:40:44 +0300

    for scrn

--- main.py
+++ main.py
@@ -11,20 +11,30 @@ import pickle
 #     black_list = pickle.load(f)
 
 text_example = ['Футбол это круто', 'Привет, я банан']
-fragments = []
+fragments = {}
 TOKEN = '5198566227:AAGeVXLtPHBNEgl5A1JAQwyL3a85gKKCCtw'
 
 bot = telebot.TeleBot(TOKEN)
 
 def addtoblock(message):
     new_word = message.text
-    print(new_word)
+    # print(new_word)
     with open('./DB/block_list.pickle', 'rb') as f:
         black_list = pickle.load(f)
     black_list.append(new_word)
-    print(black_list)
+    # print(black_list)
     with open('./DB/block_list.pickle', 'wb') as f:
         pickle.dump(black_list,f)
+    bot.send_message(message.from_user.id, 'Слово добавлено в черный список')
+
+def delfromblock(message):
+    del_word = message.text
+    with open('./DB/block_list.pickle', 'rb') as f:
+        black_list = pickle.load(f)
+    black_list.remove(del_word)
+    with open('./DB/block_list.pickle', 'wb') as f:
+        pickle.dump(black_list,f)
+    bot.send_message(message.from_user.id, 'Слово удалено из черный список')
 
 def view_news(message):
     bot.send_message(message.from_user.id, 'Вот что я подобрал для Вас')
@@ -32,17 +42,21 @@ def view_news(message):
         black_list = pickle.load(f)
     for ban_word in black_list:
         for item in text_example:
-            item = item.lower().replace(" ","")
-            for part in range(len(item)):
-                fragment = item[part: part+len(ban_word)]
-                fragments.append(fragment)
-    print(fragments)
-    for text in fragments:
-        # print(text)
-        # print(fragments)
-        if text == ban_word:
-            print(ban_word)
-    #         bot.send_message(message.from_user.id, text_example)
+            fragments[item] = []
+            new_item = item.lower().replace(" ","")
+            for part in range(len(new_item)):
+                fragment = new_item[part: part+len(ban_word)]
+                fragments[item].append(fragment)
+
+        for text in fragments.values():
+            print(text)
+            for word in text:
+                # print(word)
+                if word == ban_word:
+                    print(ban_word)
+                    key=list(fragments.keys())[list(fragments.values()).index(text)]
+                    print(key)
+                    bot.send_message(message.from_user.id,key)
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -51,7 +65,8 @@ def start(message):
 /help - посмотреть список команд
 /block - посмотреть черный список
 /addtoblock - добавить тему/слово в черный список
-/viewnews - посмотреть новости''')
+/viewnews - посмотреть новости
+/delfromblock - удалить из черного списка''')
 
 @bot.message_handler(commands=['help'])
 def help(message):
@@ -59,7 +74,8 @@ def help(message):
 /help - посмотреть список команд
 /block - посмотреть черный список
 /addtoblock - добавить тему/слово в черный список
-/viewnews - посмотреть новости''')
+/viewnews - посмотреть новости
+/delfromblock - удалить из черного списка''')
 
 @bot.message_handler(commands=['block'])
 def blocklist(message):
@@ -72,6 +88,11 @@ def blocklist(message):
 def add_to_blacklist(message):
     add_text = bot.send_message(message.from_user.id, 'Введите слово, которое хотите добавить в черный список')
     bot.register_next_step_handler(add_text, addtoblock)
+
+@bot.message_handler(commands=['delfromblock'])
+def del_from_block(message):
+    add_text = bot.send_message(message.from_user.id, 'Введить слово/тему которое хотите убрать из черного списка')
+    bot.register_next_step_handler(add_text, delfromblock)
 
 @bot.message_handler(commands = ['viewnews'])
 def news(message):
